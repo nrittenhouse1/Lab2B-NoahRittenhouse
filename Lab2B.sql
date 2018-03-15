a35c5194a8c2dc0c61ece183455eb386033f99e8
9o8o9o <welkerules99@gmail.com>
2018-03-15 10:58:43 -0600

    This

--- Lab2B.sql
+++ Lab2B.sql
@@ -9,3 +9,31 @@
 
 USE [1508_Lab2A_BooksGalore]
 GO
+
+--1.	Select the ISBN, title, publisher name and number in stock.  List the data in ISBN sequence. 
+
+SELECT 
+FROM
+
+--2.	Select the ISBN, title, suggested price, the sale price (suggested price less 10%) and the difference between the two values.
+
+--3.	Select the employee number and name of all the employees who don’t have a sale. Do not use a join.
+
+--4.	Select the total number of books and the total money earned for the company for the current year. 
+
+--5.	Select the category code, category description and the total number of books sold and the total money earned for each and every category.
+
+--6.	Select the ISBN, title and amount earned for the current year for the titles that sold more than $100(not including GST).
+
+--7.	Write the select statement to return ALL the customer names, CustomerNumbers and how many sales they have.
+
+--8.	Select The CustomerNumber, Full Name, SaleNumber, SaleDate, SubTotal, GST, Total, ISBN, Title Quantity, and Amount for Sale Numbers 3008 to 3010. 
+
+--9.	Select the Full Names and full mailing address of all the Customers and Employees. In order to create mailing labels the following 5 columns are required: 
+--		Name, Address, City, Prov, PC. DO NOT return more than 5 columns. There is no need to differentiate between Customer and Employee addresses.
+
+--10.	Select the Customer Names that have shaw.ca email addresses and have made more than 3 sales.
+
+--11.	Select the name of the Employee and the email address for the employee that has the highest individual sale for the current month. This query will be run on the last day of each month to determine the winner for that month. You must use a subquery in your solution.
+
+--12.	Select ALL the customer full names (as one column),a count of books they have purchased and the number of sales they have. Order the list alphabeticaly by Customer Last Name.
