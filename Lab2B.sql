d1ca0f95b1c5e3851602020741238b45ba530df2
9o8o9o <welkerules99@gmail.com>
2018-03-15 11:48:11 -0600

    LabWork

--- Lab2B.sql
+++ Lab2B.sql
@@ -12,20 +12,60 @@ GO
 
 --1.	Select the ISBN, title, publisher name and number in stock.  List the data in ISBN sequence. 
 
-SELECT 
-FROM
+SELECT ISBN, Title, P.Name AS 'Publisher Name', NumberInStock
+FROM Books B
+	INNER JOIN Publishers P ON B.PublisherCode = P.PublisherCode
+ORDER BY ISBN
 
---2.	Select the ISBN, title, suggested price, the sale price (suggested price less 10%) and the difference between the two values.
+--2.	The company is having a 10% sale on books in the category “Computers”. 
+--		Select the ISBN, title, suggested price, the sale price (suggested price less 10%) and the difference between the two values.
+
+SELECT ISBN, Title, SuggestedPrice AS 'Suggested Price', 
+	SuggestedPrice - (SuggestedPrice * 0.10) AS 'Sale Price', /*Sale Price*/
+	SuggestedPrice - (SuggestedPrice - (SuggestedPrice * 0.10)) AS 'Difference' /*Difference*/
+FROM Books B
+	INNER JOIN Categories C ON B.CategoryCode = C.CategoryCode
+WHERE C.Description = 'Computers'
 
 --3.	Select the employee number and name of all the employees who don’t have a sale. Do not use a join.
 
+SELECT EmployeeNumber, FirstName + ' ' + LastName AS 'Employee Name'
+FROM Employees
+WHERE EmployeeNumber NOT IN /* Employees who are not in the sales category, meaning they made no sales the lazy good for nothings*/
+	(
+		SELECT EmployeeNumber
+		FROM Sales
+	)
+
 --4.	Select the total number of books and the total money earned for the company for the current year. 
+
+SELECT SUM(NumberInStock) AS 'Total Books', SUM(S.Total) AS 'Total Income'
+FROM Books B
+	INNER JOIN SaleDetails SD ON B.ISBN = SD.ISBN
+	INNER JOIN Sales S ON SD.SaleNumber = S.SaleNumber
+WHERE YEAR(S.Date) = YEAR(GETDATE())
 
 --5.	Select the category code, category description and the total number of books sold and the total money earned for each and every category.
 
+SELECT C.categoryCode AS 'Category Code', C.Description, SUM(SD.Quantity) AS 'Total Books Sold', SUM(S.Total) AS 'Total Income'
+FROM Categories C
+	INNER JOIN Books B ON B.CategoryCode = C.categoryCode
+	INNER JOIN SaleDetails SD ON SD.ISBN = B.ISBN
+	INNER JOIN Sales S On S.SaleNumber = SD.SaleNumber
+GROUP BY C.categoryCode, C.Description
+
 --6.	Select the ISBN, title and amount earned for the current year for the titles that sold more than $100(not including GST).
 
+SELECT B.ISBN, Title, SUM(SubTotal) AS 'Total Income(No GST)'
+FROM Books B
+	INNER JOIN SaleDetails SD ON SD.ISBN = B.ISBN
+	INNER JOIN Sales S On S.SaleNumber = SD.SaleNumber
+WHERE SD.Amount > 100
+GROUP BY B.ISBN, Title
+
 --7.	Write the select statement to return ALL the customer names, CustomerNumbers and how many sales they have.
+
+
 
 --8.	Select The CustomerNumber, Full Name, SaleNumber, SaleDate, SubTotal, GST, Total, ISBN, Title Quantity, and Amount for Sale Numbers 3008 to 3010. 
 
